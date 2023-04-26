6abc757e0f2b934884a425edef77a5d4820fa284
Manuel Apolinar Castillo <apolinar121070@gmail.com>
2023-04-26 02:58:38 -0600

    Modificacion de los hechos del precio de la transmision

--- autos.pl
+++ autos.pl
@@ -39,19 +39,33 @@ combustible(carroceria("NA", convertible), gasolina).
 precioCarroceria(carroceria("NA", convertible), 1500).
 
 
-precioTransmision(estandar, 2500).
-precioTransmision(automatico, 5000).
+% precioTransmision(estandar, 2500).
+% precioTransmision(automatico, 5000).
+
+
+
+precioTransmision(
+    transmision(carroceria("NA", convertible),  estandar),
+    2500
+).
+
+precioTransmision(
+    transmision(carroceria("NA", convertible),  automatico),
+    5000
+).
 
 % metodo para calcular el precio del carro
 % de acuerdo a las variacines de combustible y transmision
 calcularPrecio(Generacion, TipoCarroceria):- 
     Carroceria = carroceria(Generacion, TipoCarroceria),
+    Transmision = transmision(Carroceria, TipoTransmision),
 
     carroceria(Generacion, TipoCarroceria), 
 
     combustible(Carroceria, Combustible),
-    transmision(Carroceria, Transmision),
+    transmision(Carroceria, TipoTransmision),
     precioCarroceria(Carroceria, PrecioInicial),
+
     precioTransmision(Transmision, PrecioTransmision),
 
     PrecioFinal is PrecioInicial + PrecioTransmision,
@@ -59,7 +73,7 @@ calcularPrecio(Generacion, TipoCarroceria):-
 
     write("Precio: "), write(PrecioInicial), nl,
     write("Combustible: "), write(Combustible), nl,
-    write("Transmision: "), write(Transmision), nl,
+    write("Transmision: "), write(TipoTransmision), nl,
     write("PrecioFinal: "), write(PrecioFinal), nl.
 
     
