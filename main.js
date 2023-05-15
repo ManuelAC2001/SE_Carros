cea364ec3dabec318e29e52c59dbc4530cc26ed8
Manuel Apolinar Castillo <apolinar121070@gmail.com>
2023-05-15 14:58:35 -0600

    Primera visualizacion de los datos en interfaz grafica

--- main.js
+++ main.js
@@ -10,5 +10,26 @@ async function getCarros() {
 
 this.onload = async () => {
     let carros = await getCarros()
-    carros.forEach(c => console.log(c))
+    // carros.forEach(c => console.log(c))
+
+    let carContainer = document.querySelector(".car__container")
+
+    carros.forEach(carro => {
+        carContainer.innerHTML += `
+
+        <div class="car_card">
+            <p>Marca: $${carro.marca}</p>
+            <p>Modelo: ${carro.modelo}</p>
+            <p>Generación: ${carro.generacion}</p>
+            <p>Año de salida: ${carro.anio}</p>
+            <p>Carroceria: ${carro.carroceria}</p>
+            <p>Número de puertas: ${carro.numeropuertas}</p>
+            <p>Combustible: ${carro.combustible}</p>
+            <p>Transmisión: ${carro.transmision}</p>
+        </div>
+        </br>
+        `
+
+    })
+
 }
