fc1eadbe5faf39391689a31c90471c8e70abf47a
Manuel Apolinar Castillo <apolinar121070@gmail.com>
2023-05-23 22:10:57 -0600

    creacion de slider

--- view/js/main.js
+++ view/js/main.js
@@ -1,9 +1,60 @@
-this.onload = async () => {
+this.addEventListener("load", async () => {
+
     const respuestas = JSON.parse(localStorage.getItem("respuestas"));
-    const cars = await filterCars(respuestas)
-    showCar(cars[0])
+    const filteredCars = await filterCars(respuestas)
+
+    const sliderBack = document.getElementById("sliderBack")
+    const sliderNext = document.getElementById("sliderNext")
+
+    let counterCar = 0
+    let carroActual = filteredCars[counterCar]
+
+    showCar(carroActual)
+
+    sliderNext.addEventListener("click", () => {
+
+        counterCar++
+        carroActual = filteredCars[counterCar]
+
+        if(!carroActual){
+            counterCar = filteredCars.length - 1
+            carroActual = filteredCars[counterCar]
+            return
+        }
+        showCar(carroActual)
+        console.log(carroActual);
+        
+    })
     
+    sliderBack.addEventListener("click", () => {
+        
+        counterCar--
+        carroActual = filteredCars[counterCar]
+        
+        if(!carroActual){
+            counterCar = 0
+            carroActual = filteredCars[counterCar]
+            return
+        }
+        
+        showCar(carroActual)
+    })
+
+})
+
+const setSlider = cars => {
+    if (!cars) return
+
+    const numberOfCars = cars.length
+
+    if (numberOfCars <= 1) {
+        sliderNext.style.visibility = "hidden"
+        sliderBack.style.visibility = "hidden"
+    }
+
 }
+
+
 
 const getCars = async () => {
     let cars = []
@@ -38,17 +89,26 @@ const showCar = async car => {
     carModelHTML.innerText = `${car.modelo} ${car.generacion}`
     carModelHTML.innerHTML += ` <span>${car.anio}</span>`
 
-    const carFeaturesList = [
-        car.carroceria,
-        car.numeropuertas,
-        car.combustible,
-        car.transmision,
-        car.precio
-    ]
+    const { carroceria, numeropuertas, combustible, transmision, precio } = car
 
-    Array.from(carFeaturesHTML.children).forEach((featureDIV, key) => {
+    const carFeaturesList = {
+        "carroceria": carroceria,
+        "numeropuertas": numeropuertas,
+        "combustible": combustible,
+        "transmision": transmision,
+        "precio": precio
+    }
 
+    Object.entries(carFeaturesList).forEach(([feature, value], iterator) => {
+
+        const featureDIV = carFeaturesHTML.children[iterator]
         const featureText = featureDIV.querySelector("P")
-        featureText.innerText += `${carFeaturesList[key]}`
+
+        if(feature == "precio") {
+            featureText.innerHTML = `$${value}`
+        }
+        
+        featureText.innerHTML = `${value}`
     })
+
 }
