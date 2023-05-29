670378010f9995a097d550edd94c73b2f2fe12cd
Manuel Apolinar Castillo <apolinar121070@gmail.com>
2023-05-29 01:31:04 -0600

    correcion del anio de generacion del modelo aveo

--- base/knowledge/cars.pl
+++ base/knowledge/cars.pl
@@ -3,6 +3,7 @@
 % marcas disponibles
 marca(mazda).
 marca(audi).
+marca(chevrolet).
 
 
 % modelo mx-5
@@ -10,6 +11,9 @@ modelo(mazda, "mx-5").
 
 % modelo A3
 modelo(audi, "A3").
+
+% modelo aveo
+modelo(chevrolet, "aveo").
 
 % generaciones de cada modelo mx-5
 generacion("mx-5", "NA").
@@ -22,6 +26,12 @@ generacion("A3", "8P").
 generacion("A3", "8V").
 generacion("A3", "8Y").
 
+% generaciones del modelo aveo
+generacion("aveo", "T200").
+generacion("aveo", "T250").
+generacion("aveo", "T300").
+generacion("aveo", "Sail").
+generacion("aveo", "310C").
 
 % anio de fabricacion del modelo mx-5
 anioFabricacion("NA", 1989).
@@ -34,6 +44,12 @@ anioFabricacion("8P", 2003).
 anioFabricacion("8V", 2012).
 anioFabricacion("8Y", 2020).
 
+% anio de fabricacion del modelo aveo
+anioFabricacion("T200", 2002).
+anioFabricacion("T250", 2005).
+anioFabricacion("T300", 2012).
+anioFabricacion("Sail", 2017).
+anioFabricacion("310C", 2023).
 
 % carroceria de las generaciones del modelo mx-5
 carroceria("NA", convertible).
@@ -52,6 +68,22 @@ carroceria("8V", sedan).
 
 carroceria("8Y",sedan).
 
+% carroceria de las generaciones del modelo aveo
+carroceria("T200", hatchback).
+carroceria("T200", sedan).
+
+carroceria("T250", hatchback).
+carroceria("T250", sedan).
+
+carroceria("T300", hatchback).
+carroceria("T300", sedan).
+
+carroceria("Sail", sedan).
+
+carroceria("310C", hatchback).
+carroceria("310C", sedan).
+
+
 
 % fotos de la carroceria de la generacion del modelo mx-5 con su respectiva carroceria
 fotoCarroceria(carroceria("NA", convertible), "mx5na.png").
@@ -63,7 +95,6 @@ fotoCarroceria(carroceria("NC", convertible), "mx5nc.png").
 fotoCarroceria(carroceria("8L", hatchback), "A38Lhatchback.png").
 fotoCarroceria(carroceria("8L", sedan), "A38Lsedan.webp").
 
-
 fotoCarroceria(carroceria("8P", hatchback),   "A38Phatchback.webp").
 fotoCarroceria(carroceria("8P", convertible), "A38Pconvertible.png").
 
@@ -72,6 +103,20 @@ fotoCarroceria(carroceria("8V", hatchback), "A38Vhatchback.webp").
 
 fotoCarroceria(carroceria("8Y",sedan), "A38Ysedan.png").
 
+% fotos de la carroceria de la generacion del modelo aveo con su respectiva carroceria
+fotoCarroceria(carroceria("T200", hatchback), "AveoT200Hatchback.png").
+fotoCarroceria(carroceria("T200", sedan), "AveoT200Sedan.png").
+
+fotoCarroceria(carroceria("T250", hatchback), "").
+fotoCarroceria(carroceria("T250", sedan), "").
+
+fotoCarroceria(carroceria("T300", hatchback), "").
+fotoCarroceria(carroceria("T300", sedan), "").
+
+fotoCarroceria(carroceria("Sail", sedan), "").
+
+fotoCarroceria(carroceria("310C", hatchback), "").
+fotoCarroceria(carroceria("310C", sedan), "").
 
 % numero de puertas que tiene cada generacion del modelo mx5 dependiendo de su carroceria
 numero_puertas(carroceria("NA", convertible), 2).
@@ -90,6 +135,22 @@ numero_puertas(carroceria("8V",hatchback), 2).
 numero_puertas(carroceria("8V",sedan),     4).
 
 numero_puertas(carroceria("8Y",sedan), 4).
+
+
+% numero de puertas que tiene cada generacion del modelo aveo dependiendo de su carroceria
+numero_puertas(carroceria("T200", hatchback), 4).
+numero_puertas(carroceria("T200", sedan), 4).
+
+numero_puertas(carroceria("T250", hatchback), 4).
+numero_puertas(carroceria("T250", sedan), 4).
+
+numero_puertas(carroceria("T300", hatchback), 4).
+numero_puertas(carroceria("T300", sedan), 4).
+
+numero_puertas(carroceria("Sail", sedan), 4).
+
+numero_puertas(carroceria("310C", hatchback), 4).
+numero_puertas(carroceria("310C", sedan), 4).
 
 
 % transmisiones disponibles que tiene cada generacion junto con su carroceria del modelo mx-5
@@ -122,6 +183,29 @@ transmision(carroceria("8V", sedan)     , automatico).
 transmision(carroceria("8Y",sedan), estandar).
 transmision(carroceria("8Y",sedan), automatico).
 
+
+% transmisiones disponibles que tiene cada generacion junto con su carroceria del modelo aveo
+transmision(carroceria("T200", hatchback), estandar).
+transmision(carroceria("T200", sedan), estandar).
+
+transmision(carroceria("T250", hatchback), estandar).
+transmision(carroceria("T250", sedan), estandar).
+transmision(carroceria("T250", hatchback), automatico).
+transmision(carroceria("T250", sedan), automatico).
+
+transmision(carroceria("T300", hatchback), estandar).
+transmision(carroceria("T300", hatchback), automatico).
+transmision(carroceria("T300", sedan), estandar).
+transmision(carroceria("T300", sedan), automatico).
+
+transmision(carroceria("Sail", sedan), estandar).
+transmision(carroceria("Sail", sedan), automatico).
+
+transmision(carroceria("310C", hatchback), estandar).
+transmision(carroceria("310C", sedan), estandar).
+transmision(carroceria("310C", hatchback), automatico).
+transmision(carroceria("310C", sedan), automatico).
+
 % combustibles disponibles que tiene cada generacion junto con su carroceria del modelo A3
 combustible(carroceria("NA", convertible), gasolina).
 
@@ -142,6 +226,22 @@ combustible(carroceria("8V", sedan),     gasolina).
 
 combustible(carroceria("8Y",sedan), gasolina).
 
+
+% combustibles disponibles que tiene cada generacion junto con su carroceria del modelo aveo
+combustible(carroceria("T200", hatchback),gasolina).
+combustible(carroceria("T200", sedan),gasolina).
+
+combustible(carroceria("T250", hatchback),gasolina).
+combustible(carroceria("T250", sedan),gasolina).
+
+combustible(carroceria("T300", hatchback),gasolina).
+combustible(carroceria("T300", sedan),gasolina).
+
+combustible(carroceria("Sail", sedan),gasolina).
+
+combustible(carroceria("T310", hatchback),gasolina).
+combustible(carroceria("T310", sedan),gasolina).
+
 % precio de la carroceria junto a su generacion para el modelo mx-5
 precioCarroceria(carroceria("NA", convertible), 1500).
 precioCarroceria(carroceria("NB", convertible), 2300).
@@ -159,6 +259,23 @@ precioCarroceria(carroceria("8V", hatchback), 10_320).
 precioCarroceria(carroceria("8V", sedan),    10_755).
 
 precioCarroceria(carroceria("8Y", sedan), 12_750).
+
+
+% precio de la carroceria junto a su generacion para el modelo aveo
+precioCarroceria(carroceria("T200", hatchback), 10_500).
+precioCarroceria(carroceria("T200", sedan), 10_550).
+
+precioCarroceria(carroceria("T250", hatchback), 10_600).
+precioCarroceria(carroceria("T250", sedan), 10_500).
+
+precioCarroceria(carroceria("T300", hatchback), 10_625).
+precioCarroceria(carroceria("T300", sedan), 10_650).
+
+precioCarroceria(carroceria("Sail", sedan), 10_675).
+
+precioCarroceria(carroceria("310C", hatchback), 10_700).
+precioCarroceria(carroceria("310C", sedan), 10_720).
+
 
 
 % precio de la transmision para cada generacion junto con su carroceria para el modelo mx-5
@@ -243,6 +360,62 @@ precioTransmision(transmision(carroceria("8Y",sedan), automatico),
 ).
 
 
+% precio de la transmision para cada generacion junto con su carroceria para el modelo aveo
+precioTransmision(combustible(carroceria("T200", hatchback), estandar), 
+    25_250
+).
+precioTransmision(combustible(carroceria("T200", sedan), automatico), 
+    25_500
+).
+
+
+precioTransmision(transmision(carroceria("T250", hatchback), estandar), 
+    25_500
+).
+precioTransmision(transmision(carroceria("T250", sedan), estandar), 
+    25_550
+).
+precioTransmision(transmision(carroceria("T250", hatchback), automatico), 
+    25_570
+).
+precioTransmision(transmision(carroceria("T250", sedan), automatico), 
+    25_600
+).
+
+
+precioTransmision(transmision(carroceria("T300", hatchback), estandar), 
+    25_500
+).
+precioTransmision(transmision(carroceria("T300", hatchback), automatico), 
+    25_600
+).
+precioTransmision(transmision(carroceria("T300", sedan), estandar), 
+    25_700
+).
+precioTransmision(transmision(carroceria("T300", sedan), automatico), 
+    25_800
+).
+
+precioTransmision(transmision(carroceria("Sail", sedan), estandar), 
+    25_600
+).
+precioTransmision(transmision(carroceria("Sail", sedan), automatico), 
+    25_645
+).
+
+
+precioTransmision(transmision(carroceria("310C", hatchback), estandar), 
+    28_350
+).
+precioTransmision(transmision(carroceria("310C", sedan), estandar), 
+    28_375
+).
+precioTransmision(transmision(carroceria("310C", hatchback), automatico), 
+    28_400
+).
+precioTransmision(transmision(carroceria("310C", sedan), automatico), 
+    28_450
+).
 
 % precio del combustible para cada generacion junto con su carroceria para el modelo mx-5
 precioCombustible(
@@ -299,6 +472,33 @@ precioCombustible(combustible(carroceria("8V", sedan),     gasolina),
 
 precioCombustible(combustible(carroceria("8Y",sedan), gasolina), 
     62_300
+).
+
+
+% precio del combustible para cada generacion junto con su carroceria para el modelo A3
+precioCombustible(combustible(carroceria("T200", hatchback),gasolina), 
+    45_350
+).
+precioCombustible(combustible(carroceria("T200", sedan),gasolina), 
+    45_450
+).
+
+precioCombustible(combustible(carroceria("T250", hatchback),gasolina), 
+    45_350
+).
+precioCombustible(combustible(carroceria("T250", sedan),gasolina), 
+    45_500
+).
+
+precioCombustible(combustible(carroceria("Sail", sedan),gasolina), 
+    45_300
+).
+
+precioCombustible(combustible(carroceria("T300", hatchback),gasolina), 
+    45_600
+).
+precioCombustible(combustible(carroceria("T300", sedan),gasolina), 
+    45_650
 ).
 
 % FIN DE LA BASE DE CONOCIMIENTOS
